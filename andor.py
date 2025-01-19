94afb71ab8fd085c2a05ae1a099f800e5a45b0ae
mirak2010 <mirakhmad0101@yahoo.com>
2025-01-19 11:20:58 +0500

    Done

---
+++ andor.py
@@ -0,0 +1,18 @@
+a=10
+b=2
+c=0
+if a and b and c:
+    print("All numbers have some value")
+else:
+    print("One of the letters doesn't have a value!")
+a=11
+b=-2
+c=0
+if a>0 or b>0:
+    print("One of them is greater than zero")
+else:
+    print("None of them is greater than zero")
+if b>0 or c>0:
+    print("One of them is greater than zero")
+else:
+    print("None of them is greater than zero")
