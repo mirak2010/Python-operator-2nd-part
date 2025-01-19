e4b9464466091dcd3d8e3401ca0aa1c632caae40
mirak2010 <mirakhmad0101@yahoo.com>
2025-01-19 11:32:50 +0500

    #

---
+++ not.py
@@ -0,0 +1,19 @@
+a=10
+b=14
+c=14
+print(a !=b)
+print(b !=c)
+a="python"
+b="coding"
+if a !=b:
+    print(a ,"and", b ,"are different")
+a=4
+b=6
+if (a==4) != (b==4):
+    print("Hi")
+
+a=int(input("Please enter a number "))
+if a%2 !=0:
+    print("The number isn't even")
+else:
+    print("The number is even")
