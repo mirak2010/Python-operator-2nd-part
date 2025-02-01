35309a9282fd9e303c0fd90f1876c97e2aa5347b
mirak2010 <mirakhmad0101@yahoo.com>
2025-02-01 13:28:54 +0500

    #

---
+++ alphachecker.py
@@ -0,0 +1,5 @@
+letter=input("Please enter a letter(lowercase please): ")
+if letter=="a"or"b"or"c"or"d"or"e"or"f"or"g"or"h"or"i"or"j"or"k"or"l"or"m"or"n"or"o"or"p"or"q"or"r"or"s"or"t"or"u"or"v"or"w"or"x"or"y"or"z":
+    print("This is a letter of the alphabet!")
+else:
+    print("This is not the letter of the alphabet!")
